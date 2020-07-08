766340d2b088511be5899529a798a48553ef713f
Amit Wolf <amitthewolf@hotmail.com>
2020-07-08 18:49:18 +0300

    update cors 3

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const cors = require("cors");
 const session = require("client-sessions");
 
 // routes
