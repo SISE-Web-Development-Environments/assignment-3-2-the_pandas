8d9a6081ed673c09072acd4f0dee8cc4c24aa22a
IdanAlbilia <54328209+IdanAlbilia@users.noreply.github.com>
2020-07-11 19:03:57 +0300

    auth updated

--- routes/auth.js
+++ routes/auth.js
@@ -13,9 +13,9 @@ router.post("/register", async function (req, res) {
   let user_data = req.body;
   let user = await auth_util.getSpecificUserFromDb(user_data.userName);
   if (user.length == 1)
-    res.send(
-      "Email address or Username already exists. Login or use a different Email address or username."
-    );
+    res
+      .sendStatus(400)
+      .send("Username already exists. Login or use a different username.");
   else {
     try {
       let HashPass = await auth_util.checkPasswordandhash(
@@ -30,19 +30,18 @@ router.post("/register", async function (req, res) {
   }
 });
 
-
-router.post("/login",async function(req, res){
-    let userName = req.body.userName;
-    let pass = req.body.password;
-    try {
-        let user = await auth_util.attemptLogin(userName,pass)
-        req.session.user_id = user[0].user_id;
-        res.status(200).send({ message: "login succeeded", success: true });
-      } catch (error) {
-        console.log(error);
-        res.sendStatus(404);
-      }
-    });
+router.post("/login", async function (req, res) {
+  let userName = req.body.userName;
+  let pass = req.body.password;
+  try {
+    let user = await auth_util.attemptLogin(userName, pass);
+    req.session.user_id = user[0].user_id;
+    res.status(200).send({ message: "login succeeded", success: true });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(404);
+  }
+});
 
 router.post("/Logout", function (req, res) {
   req.session.reset(); // reset the session info --> send cookie when  req.session == undefined!!
