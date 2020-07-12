403fbb25effe8e30f687edc6989ae8b65c172e5e
Amit Wolf <amitthewolf@hotmail.com>
2020-07-12 18:44:39 +0300

    wierd stuff

--- index.js
+++ index.js
@@ -47,6 +47,14 @@ app.get("/alive",(req,res) => {
     res.send("Alive");
 });
 
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+    next();
+  });
+
 //routing
 app.use("/users",users);
 app.use("/recipes",recipes);
@@ -56,6 +64,7 @@ app.use(auth);
 app.use((req,res) => {
     res.sendStatus(404);
 });
+
 
 app.listen(port, () => {
     console.log('Example app listening on port '+port+'!');
